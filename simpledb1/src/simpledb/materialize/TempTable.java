01661b72af0e445e55940286aecc76785134fcd3
mustafautku <mustafautku@myasus>
2016-03-01 14:21:26 +0200

    WindowUpdateScan cikardim. Onun yerine ChunScan'i genislettim. Test1 evvelkine benzer şekilde window bolgesinin kullanılması ile ililgi bir ornek..

--- simpledb1/src/simpledb/materialize/TempTable.java
+++ simpledb1/src/simpledb/materialize/TempTable.java
@@ -2,49 +2,84 @@ package simpledb.materialize;
 
 import simpledb.tx.Transaction;
 import simpledb.record.*;
+import simpledb.buffer.PageFormatter;
 import simpledb.query.*;
 
 /**
- * A class that creates temporary tables.
- * A temporary table is not registered in the catalog.
- * The class therefore has a method getTableInfo to return the 
- * table's metadata. 
+ * A class that creates temporary tables. A temporary table is not registered in
+ * the catalog. The class therefore has a method getTableInfo to return the
+ * table's metadata.
+ * 
  * @author Edward Sciore
  */
 public class TempTable {
-   private static int nextTableNum = 0;
-   private TableInfo ti;
-   private Transaction tx;
-   
-   /**
-    * Allocates a name for for a new temporary table
-    * having the specified schema.
-    * @param sch the new table's schema
-    * @param tx the calling transaction
-    */
-   public TempTable(Schema sch, Transaction tx) {
-      String tblname = nextTableName();
-      ti = new TableInfo(tblname, sch);
-      this.tx = tx;
-   }
-   
-   /**
-    * Opens a table scan for the temporary table.
-    */
-   public UpdateScan open() {
-      return new TableScan(ti, tx);
-   }
-   
-   /**
-    * Return the table's metadata.
-    * @return the table's metadata
-    */
-   public TableInfo getTableInfo() {
-      return ti;
-   }
-   
-   private static synchronized String nextTableName() {
-      nextTableNum++;
-      return "temp" + nextTableNum;
-   }
+	private static int nextTableNum = 0;
+	private TableInfo ti;
+	private Transaction tx;
+
+	/**
+	 * Allocates a name for for a new temporary table having the specified
+	 * schema.
+	 * 
+	 * @param sch
+	 *            the new table's schema
+	 * @param tx
+	 *            the calling transaction
+	 */
+	/**
+	 * General usage of temp file. Do not need to access each pages of file.
+	 * @param sch
+	 * @param tx
+	 */
+	public TempTable(Schema sch, Transaction tx) {  
+		String tblname = nextTableName();
+		ti = new TableInfo(tblname, sch);
+		this.tx = tx;
+		/*
+		 * TempTable' ilk bloðu formatlanmýþ olarak ekliyor. MBSort'da
+		 * RecordPAge seviyesinde eriþmek için yaptýk. Evvelden temp
+		 * dosyasýndaki RecordPage'lara eriþmiyorduk. TableScan(RecordFile)
+		 * gerekli formatlamalarý yapýyordu.
+		 */
+		PageFormatter fmtr = new RecordFormatter(ti);
+		tx.append(ti.fileName(), fmtr);
+	}
+	
+	/**
+	 * used for "formatted" CHUNK area including "size" number of pages. You can scan/update the chunk records within each specific recordpage.
+	 * @param sch
+	 * @param size
+	 * @param tx
+	 */
+	  
+	public TempTable(Schema sch, int size, Transaction tx) {
+		String tblname = nextTableName();
+		ti = new TableInfo(tblname, sch);
+		this.tx = tx;
+		// TempTable'a page'ler formatlanmýþ olarak ekleniyor.
+		PageFormatter fmtr = new RecordFormatter(ti);
+		for (int i = 0; i < size; i++)
+			tx.append(ti.fileName(), fmtr);
+	}
+
+	/**
+	 * Opens a table scan for the temporary table.
+	 */
+	public UpdateScan open() {
+		return new TableScan(ti, tx);
+	}
+
+	/**
+	 * Return the table's metadata.
+	 * 
+	 * @return the table's metadata
+	 */
+	public TableInfo getTableInfo() {
+		return ti;
+	}
+
+	private static synchronized String nextTableName() {
+		nextTableNum++;
+		return "temp" + nextTableNum;
+	}
 }
--- simpledb1/src/testskyline/Test1.java
+++ simpledb1/src/testskyline/Test1.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import simpledb.materialize.TempTable;
 import simpledb.metadata.MetadataMgr;
-import simpledb.multibuffer.WindowUpdateScan;
+import simpledb.multibuffer.ChunkScan;
+import simpledb.query.Plan;
+import simpledb.query.Scan;
+import simpledb.query.TablePlan;
 import simpledb.query.TableScan;
 import simpledb.record.RID;
-import simpledb.record.RecordFile;
+import simpledb.record.Schema;
 import simpledb.record.TableInfo;
 import simpledb.server.SimpleDB;
 import simpledb.tx.Transaction;
@@ -33,89 +36,62 @@ public class Test1 {
 											// kayýt var.
 		Transaction tx = new Transaction();
 		MetadataMgr md = SimpleDB.mdMgr();
-
+		TableInfo ti = md.getTableInfo("input", tx);
+		Schema sch = ti.schema();
+		
+		int counterInWindow = 0;
+		int counterInTemp = 0;
 		/*
 		 * input tablosunundaki kayýtlarý sýrayla okuyoruz. Burda 1 tampon(page)
 		 * kullanýyor.
 		 */
-		int size = tx.size("input.tbl");
-		TableInfo ti = md.getTableInfo("input", tx);
-		int recLen = ti.recordLength();
-		RecordFile input = new RecordFile(ti, tx);
-
+			
+		Plan p=new TablePlan("input",tx);
+		Scan input = p.open();
+		input.next();
 		/*
 		 * Ana hafizadaki WINDOW bölgesinin set edilmesi: 5 tampon yer kapliyor.
 		 * Icerisinde formatlanmýþ boþ slotlar var. Artik bu bolgeyi
 		 * kullanabiliriz.
 		 */
-		TableInfo tiWindow = new TableInfo("tempWindowFile", ti.schema()); // dosya
-																			// ismi
-																			// "temp"
-																			// ile
-																			// baslasin
-																			// ta
-																			// ki
-																			// system
-																			// yeni
-																			// basladiginda
-																			// bu
-																			// dosyayý
-																			// silsin..Bkz.
-																			// FileMgr
-																			// constructor
-		WindowUpdateScan window = new WindowUpdateScan(tiWindow, 0, 4, tx);
-
+		ChunkScan window= new ChunkScan(5, sch, tx);
 		/*
 		 * TEMP dosyamýz: Window'a sýðmayanlarý buraya yazacagiz..Geniþlerken
 		 * ayný anda 2 tampon yer kaplýyor..
 		 */
-		TempTable temp = new TempTable(ti.schema(), tx);
-		TableScan tempfile = (TableScan) temp.open();
+		TempTable outputfile = new TempTable(ti.schema(), tx);
+		TableScan output = (TableScan) outputfile.open();
 
 		// BufferMgr bm=SimpleDB.bufferMgr(); // debug amacli isterseniz tampon
 		// havuzuna bakin doðru taksimat olmus mu?
 
 		/*
 		 * Bu ornekte input dosyasýndan WINDOW sýðacak kadar kayýt okuyoruz.
-		 * Sonra Window dolduktan sonra diðer kayýtlarý tempfile'a yazýyoruz.
+		 * Sonra Window dolduktan sonra diðer kayýtlarý tempfile'a yazacagiz.
 		 */
-		int A = 0, B = 0;
-		System.out.println("Window'da olanlar"); // TOPLAM 33* 5 = 198 tane
-													// olmalý.
-		int counterInWindow = 0;
-		int counterInTemp = 0;
-
-		while (input.next()) {
-			if (window.insert()) {
-				A = input.getInt("A");
-				B = input.getInt("B");
-				window.setInt("A", A);
-				window.setInt("B", B);
-				System.out.println(A + " " + B);
-				counterInWindow++;
-			} else
+	
+		while (window.insertFromScan(input)){ // copy data from input to chunk area
+			counterInWindow++;
+			if (!input.next()) {
 				break;
+			}
 		}
-		System.out.println("Toplam window kayýtlarýnýn sayýsý: "
-				+ (counterInWindow));
+		System.out.println("Window'da olanlar: " + counterInWindow); // TOPLAM 33* 5 = 165 tane olmalý.
 
-		System.out.println("Temp dosyasýna gidenler");
-		tempfile.insert(); // en son okuduðumuzu yazamamistik. Once onu
+		
+		output.insert(); // en son okuduðumuzu yazamamistik. Once onu
 							// yazýyoruz..
-		tempfile.setInt("A", A);
-		tempfile.setInt("B", B);
-		System.out.println(A + " " + B);
+		output.setInt("A", input.getInt("A"));
+		output.setInt("B", input.getInt("B"));
 		counterInTemp++;
-		while (input.next()) { // tempfile icin bir sýnýr yok. istediði kadar
+		
+		while (input.next()) { // output icin bir sýnýr yok. istediði kadar
 								// büyüyebiir..
-			tempfile.insert(); // tempfile genislerken 2 tane tampon tutuyor. O
+			output.insert(); // tempfile genislerken 2 tane tampon tutuyor. O
 								// yuzden Windoe size'i 5 yapmak zorunda
-								// kaldik..
-			A = input.getInt("A");
-			B = input.getInt("B");
-			tempfile.setInt("A", A);
-			tempfile.setInt("B", B);
-			System.out.println(A + " " + B);
+								// kaldik..			
+			output.setInt("A", input.getInt("A"));
+			output.setInt("B", input.getInt("B"));
 			counterInTemp++;
 		}
 		
@@ -131,21 +107,30 @@ public class Test1 {
 			if(window.getInt("A")<100)
 				list.add(window.getRid());			
 		}
-		System.out.println("toplam "+ list.size() + " eleman tempfile'a aktarýlýyor");
+		System.out.println("Window'daki <100 olan elemanlar: toplam "+ list.size() + " eleman tempfile'a aktarýlýyor.");
 		Iterator<RID> iter=list.iterator();
 		while(iter.hasNext()){
 			window.moveToRid(iter.next());
-			A=window.getInt("A");
-			B=window.getInt("B");
-			tempfile.insert();  //tempfile'da yer bul
-			tempfile.setInt("A", A);
-			tempfile.setInt("B", B);
-			window.delete();			
+			output.insert();  //tempfile'da yer bul
+			output.setInt("A", window.getInt("A"));
+			output.setInt("B", window.getInt("B"));
+			window.delete();		// baska bir slota gecmedik. Bulundugumuz slotu empty yapacak.	
 		}
+		
+		
+		//Simdi window'da daha az eleman kalmýþ olmasý gerek. Bakalim:
+		counterInWindow=0;
+		window.beforeFirst();
+		while(window.next()){
+			if(window.getInt("A")<100)
+				System.err.print("silememisiz maalesef.");
+			counterInWindow++;		
+		}
+		System.out.println("Window'da geride kalanlar: " + counterInWindow); // Bu ornek icin <165 tane olmalý. 
 		
 		input.close();
 		window.close();
-		tempfile.close();
+		output.close();
 		
 	}
 
